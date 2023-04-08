fb57ddb574c93b0277078348544107964f6a90cb
Matheus Pereira <matheusgp.mto@outlook.com>
2023-04-08 02:33:55 -0300

    Finalização da função mobile

--- js/acoes/pular.js
+++ js/acoes/pular.js
@@ -1,8 +1,7 @@
 import removeClassePular from "../auxiliar/removePular.js";
 
 export default function pular(evento, alvo) {
-    console.log(evento.changedTouches[0].target.dataset.pular);
-    if(evento.code === "Space" || evento.changedTouches[0].target.dataset.pular === "pular") {
+    if(evento.code === "Space") {
         alvo.classList.add("tela__mario--pular");
         removeClassePular(alvo);
     }
