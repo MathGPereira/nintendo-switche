a3d79fecbebdd16af4b45df5691a80975d25699a
Matheus Pereira <matheusgp.mto@outlook.com>
2023-04-08 02:07:05 -0300

    Adicionando um botão de pular para jogar em versão mobile

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import verificaColisao from "./auxiliar/verificarColisao.js";
 import mudaEstadoDoNintendo from "./auxiliar/mudaEstado.js";
 import ativaWindows from "./auxiliar/ativaWindows.js";
 import restart from "./auxiliar/restart.js";
+import pular from "./acoes/pular.js";
 
 const mario = document.querySelector("[data-mario]");
 const cano = document.querySelector("[data-cano]");
@@ -12,9 +13,11 @@ const tela = document.querySelector("[data-tela]");
 const ladoDireito = document.querySelector("[data-lado-direito]");
 const botaoLigar = document.querySelector("[data-ligar]");
 const resetar = document.querySelector("[data-restart]");
+const botaoPular = document.querySelector("[data-pular]");
 
 ativaWindows(mario)
 botaoAbrir.addEventListener("click", () => mudaEstadoDoNintendo(tela, ladoDireito));
 botaoLigar.addEventListener("click", () => ligaTelaMostraJogo(tela, mario, cano, nuvem));
 resetar.addEventListener("click", () => restart(mario, cano, nuvem));
+botaoPular.addEventListener("click", evento => pular(evento, mario));
 verificaColisao(mario, cano, nuvem);
