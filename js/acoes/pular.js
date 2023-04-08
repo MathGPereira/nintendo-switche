5f11bc0cd3e8a1879a37aee5193f1a2945470425
Matheus Pereira <matheusgp.mto@outlook.com>
2023-04-08 02:32:31 -0300

    Teste 3

--- js/acoes/pular.js
+++ js/acoes/pular.js
@@ -2,7 +2,7 @@ import removeClassePular from "../auxiliar/removePular.js";
 
 export default function pular(evento, alvo) {
     console.log(evento.changedTouches[0].target.dataset.pular);
-    if(evento.code === "Space" || evento.target.dataset.pular === "pular") {
+    if(evento.code === "Space" || evento.changedTouches[0].target.dataset.pular === "pular") {
         alvo.classList.add("tela__mario--pular");
         removeClassePular(alvo);
     }
